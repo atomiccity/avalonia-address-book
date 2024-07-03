d8562ec19b0cd7964c2571a1ce67cd2c37929086
Michael R. Taylor <michael@atomic.city>
2024-07-03 17:07:17 -0500

    Added navigation and some necessary packages

--- AddressBook/AddressBook/App.axaml.cs
+++ AddressBook/AddressBook/App.axaml.cs
@@ -1,8 +1,12 @@
+using System;
+using AddressBook.Services;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AddressBook.ViewModels;
 using AddressBook.Views;
+using Avalonia.SimpleRouter;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AddressBook;
 
@@ -15,21 +19,42 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        IServiceProvider services = ConfigureServices();
+        var mainViewModel = services.GetRequiredService<MainViewModel>();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = mainViewModel
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                DataContext = new MainViewModel()
+                DataContext = mainViewModel
             };
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static ServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+        
+        // Add the HistoryRouter as a service
+        services.AddSingleton<HistoryRouter<ViewModelBase>>(s => new HistoryRouter<ViewModelBase>(t => (ViewModelBase)s.GetRequiredService(t)));
+        
+        // Add ContactService as a service
+        services.AddSingleton<ContactService>();
+
+        // Add the ViewModels as a service (Main as singleton, others as transient)
+        services.AddSingleton<MainViewModel>();
+        services.AddTransient<ContactListViewModel>();
+        services.AddTransient<ContactDetailsViewModel>();
+        services.AddTransient<ContactViewModel>();
+        return services.BuildServiceProvider();
     }
 }
